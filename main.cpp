aae49dba92257c2c84b547b31137375ed24f9152
Andrew Lin <andrewlin16@gmail.com>
2015-02-23 10:46:39 -0600

    Small refactor for multithreading

--- main.cpp
+++ main.cpp
@@ -9,6 +9,31 @@
 #define ESCAPE 4
 #define MAX_ITERATIONS 255
 
+mpf_class xmin;
+mpf_class xmax;
+mpf_class ymin;
+mpf_class ymax;
+mpf_class xinc;
+mpf_class yinc;
+
+png::image<png::index_pixel> image;
+
+void render_pixel(size_t y, size_t x) {
+	mpf_class posx = xmin + x * xinc;
+	mpf_class posy = ymax - y * yinc;
+	std::complex<mpf_class> v0(posx, posy);
+	std::complex<mpf_class> v(0, 0);
+
+	int iter = 0;
+
+	while (std::norm(v) < ESCAPE && iter < MAX_ITERATIONS) {
+		v = std::pow(v, 2) + v0;
+		iter++;
+	}
+
+	image[y][x] = iter;
+}
+
 int main(int argc, char *argv[]) {
 	// check arguments
 	if (argc < 8) {
@@ -26,10 +51,10 @@ int main(int argc, char *argv[]) {
 	std::string arg_height(argv[6]);
 	std::string arg_output(argv[7]);
 
-	mpf_class xmin(arg_xmin);
-	mpf_class xmax(arg_xmax);
-	mpf_class ymin(arg_ymin);
-	mpf_class ymax(arg_ymax);
+	xmin = mpf_class(arg_xmin);
+	xmax = mpf_class(arg_xmax);
+	ymin = mpf_class(arg_ymin);
+	ymax = mpf_class(arg_ymax);
 
 	size_t dim[2];
 	dim[0] = atoi(arg_width.c_str());
@@ -41,28 +66,16 @@ int main(int argc, char *argv[]) {
 		pal[i] = png::color(i, i, i);
 	}
 
-	png::image<png::index_pixel> image(dim[0], dim[1]);
+	image = png::image<png::index_pixel>(dim[0], dim[1]);
 	image.set_palette(pal);
 
 	// render image
-	mpf_class xinc = (xmax - xmin) / dim[0];
-	mpf_class yinc = (ymax - ymin) / dim[1];
+	xinc = (xmax - xmin) / dim[0];
+	yinc = (ymax - ymin) / dim[1];
 
 	for (size_t y = 0; y < image.get_height(); ++y) {
 		for (size_t x = 0; x < image.get_width(); ++x) {
-			mpf_class posx = xmin + x * xinc;
-			mpf_class posy = ymax - y * yinc;
-			std::complex<mpf_class> v0(posx, posy);
-			std::complex<mpf_class> v(0, 0);
-
-			int iter = 0;
-
-			while (std::norm(v) < ESCAPE && iter < MAX_ITERATIONS) {
-				v = std::pow(v, 2) + v0;
-				iter++;
-			}
-
-			image[y][x] = iter;
+			render_pixel(y, x);
 		}
 	}
 
